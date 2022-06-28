d057bcf4f605dfe67adcebb3bea4a6fd282db4a7
marcellacg <marcelladacosta@gmail.com>
2022-06-28 18:48:33 -0300

    arquivos prévios

---
+++ ano.js
@@ -0,0 +1,15 @@
+function qntAnos() {
+  let dateDropdown = document.getElementById("date-dropdown"); 
+       
+  let currentYear = new Date().getFullYear();    
+  let earliestYear = 1957;     
+  
+  while (currentYear >= earliestYear) {      
+    let dateOption = document.createElement("option");          
+    dateOption.text = currentYear;      
+    dateOption.value = currentYear;        
+    dateDropdown.appendChild(dateDropdown);   
+    
+    currentYear -= 1;
+  }
+}
